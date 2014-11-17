42a64ae73ea584a3601bd0f79ea029d0bb49a8c8
fay <fayfooksie@gmail.com>
2014-11-17 01:56:43 -0600

    fixed

--- grow.js
+++ grow.js
@@ -5,18 +5,21 @@
 	scrolls appropriate distance on viewport overflow
 */
 function grow(event) {
-	var	node=this,
-		prevheight=node.scrollHeight,
-		prevscroll=window.scrollY;
-	node.style.height="";
-	requestAnimationFrame(function() {
-		var	height=node.scrollHeight+node.offsetHeight-node.clientHeight;
-		node.style.height=height+"px";
-		if(prevheight===node.scrollHeight) {
-			window.scrollTo(window.scrollX, prevscroll);
-			}
-		else if(node.offsetTop+height-window.scrollY-window.innerHeight>0) {
-			window.scrollTo(window.scrollX, prevscroll+node.scrollHeight-prevheight);
-			}
-		});
+	if(!this.dataset.grow) {
+		var	node=this,
+			prevheight=node.scrollHeight,
+			prevscroll=window.scrollY;
+		node.style.height="";
+		node.dataset.grow=requestAnimationFrame(function() {
+			delete node.dataset.grow;
+			var	height=node.scrollHeight+node.offsetHeight-node.clientHeight;
+			node.style.height=height+"px";
+			if(prevheight===node.scrollHeight) {
+				window.scrollTo(window.scrollX, prevscroll);
+				}
+			else if(node.offsetTop+height-window.scrollY-window.innerHeight>0) {
+				window.scrollTo(window.scrollX, prevscroll+node.scrollHeight-prevheight);
+				}
+			});
+		}
 	};
